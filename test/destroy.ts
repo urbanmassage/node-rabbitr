e64adf5fb2cd686c55893a75b28f64c33d56e5d8
Louy <louy08@gmail.com>
2016-01-08 14:55:06 +0200

    Add a memory leak test

--- test/destroy.ts
+++ test/destroy.ts
@@ -1,18 +1,53 @@
 import Rabbitr = require('../');
+import async = require('async');
 import {expect} from 'chai';
 
 describe('rabbitr#destroy', function() {
-  const rabbit = new Rabbitr({
-    url: process.env.RABBITMQ_URL || 'amqp://guest:guest@localhost/%2F',
-  });
-  before((done) => rabbit.whenReady(done));
-
   it('should be able to destroy an instance once initted', function(done) {
-    setTimeout(function() {
-      rabbit.destroy((err) => {
-        expect(err).to.be.undefined;
+    const rabbit = new Rabbitr({
+      url: process.env.RABBITMQ_URL || 'amqp://guest:guest@localhost/%2F',
+    });
+    rabbit.whenReady(() => {
+      setTimeout(function() {
+        rabbit.destroy((err) => {
+          done(err);
+        });
+      }, 200);
+    });
+  });
+
+  let ifGcIt = global.gc ? it : it.skip;
+
+  ifGcIt('doesn\'t leak', function(done) {
+    function runCycle(done: (err: Error) => void) {
+      const rabbit = new Rabbitr({
+        url: process.env.RABBITMQ_URL || 'amqp://guest:guest@localhost/%2F',
+      });
+      rabbit.whenReady(() => {
+        setTimeout(function() {
+          rabbit.destroy(err => {
+            try {
+              global.gc();
+            } catch (e) {
+              return done(new Error(`You must run the test with 'mocha -gc'`));
+            }
+            done(err);
+          });
+        }, 200);
+      });
+    }
+
+    // run a cycle before so we get accurate measures.
+    runCycle(err => {
+      if (err) return done(err);
+      let {heapUsed} = process.memoryUsage();
+
+      async.times<void>(10, (n, done) => runCycle(<any>done), function(err) {
+        if (err) return done(err);
+
+        expect(process.memoryUsage().heapUsed).to.be.closeTo(heapUsed, heapUsed * 0.1);
         done();
       });
-    }, 200);
+    });
   });
 });
