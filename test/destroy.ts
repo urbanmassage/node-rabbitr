fe984f2ca1d918dbbfa6bb1efb8ac741e7e4bdce
Louy <louy08@gmail.com>
2016-01-11 10:57:21 +0200

    Add a pubsub listener to destroy test case

--- test/destroy.ts
+++ test/destroy.ts
@@ -1,13 +1,21 @@
 import Rabbitr = require('../');
 import async = require('async');
 import {expect} from 'chai';
+const uuid = require('uuid');
 
 describe('rabbitr#destroy', function() {
   it('should be able to destroy an instance once initialized', function(done) {
+    const exchangeName = uuid.v4() + '.test';
+    const queueName = uuid.v4() + '.test';
+
     const rabbit = new Rabbitr({
       url: process.env.RABBITMQ_URL || 'amqp://guest:guest@localhost/%2F',
     });
     rabbit.whenReady(() => {
+      rabbit.subscribe(queueName);
+      rabbit.bindExchangeToQueue(exchangeName, queueName);
+      rabbit.on(exchangeName, ({ack}) => ack());
+
       setTimeout(function() {
         rabbit.destroy((err) => {
           done(err);
