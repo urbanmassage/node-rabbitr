b860c8905be7a7f36b08078d71b504444aff0940
Louy <louy08@gmail.com>
2016-07-09 20:19:29 +0100

    Add test for rpc whitelisting

---
+++ test/debug.ts
@@ -0,0 +1,51 @@
+import Rabbitr = require('../');
+import {expect} from 'chai';
+import {v4} from 'node-uuid';
+
+describe('debug', function() {
+  afterEach(() => process.env.RABBITR_DEBUG = '');
+
+  it('should not skip whitelisted rpc', () => {
+    const queueName = v4() + '.rpc_test';
+    const rpcQueueName = `rpc.${queueName}`;
+
+    process.env.RABBITR_DEBUG = `rpc.${queueName}`;
+
+    const rabbit = new Rabbitr({
+      url: process.env.RABBITMQ_URL || 'amqp://guest:guest@localhost/%2F',
+    });
+
+    const response = {test: 1};
+
+    rabbit.rpcListener(queueName, function(message, cb) {
+      cb(null, response);
+    });
+
+    return rabbit.rpcExec(queueName, {})
+      .then(message => {
+        expect(message.data).to.deep.equal(response);
+      });
+  });
+
+  it('should skip non-whitelisted rpc', function(done) {
+    const queueName = v4() + '.rpc_test';
+
+    process.env.RABBITR_DEBUG = `rpc.test.1234`;
+
+    const rabbit = new Rabbitr({
+      url: process.env.RABBITMQ_URL || 'amqp://guest:guest@localhost/%2F',
+    });
+
+    rabbit.rpcListener(queueName, function (message, cb) {
+      cb(new Error('Got a message on non-whitelisted queue'));
+    });
+
+    return rabbit.rpcExec(queueName, {}, { timeout: 50 })
+      .then(message => {
+        expect.fail('Got a successful response somehow');
+      }, err => {
+        expect(err).to.be.an.instanceOf(Error);
+        expect(err).to.have.property('name').that.equals('TimeoutError');
+      });
+  });
+});
